c263abdb3fcdfc320d388f6f552d423cbea99f69
sbadalyan <s.badalyan@yahoo.com>
2016-04-15 20:26:41 +0400

    Added files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+document.addEventListener('deviceready', function(){
+    var watchId = null;
+    var start = document.querySelector('.start');
+    start.addEventListener('click', function(){
+        var options = {
+                frequency: 200
+            }
+        navigator.compass.watchHeading(
+            function(heading){
+                var element = document.getElementById('heading');
+                element.innerHTML = 'Heading: ' + heading.magneticHeading;
+                var rotation = Math.round(heading.magneticHeading)+'deg';
+                var needle = document.querySelector('.needle');
+                needle.style.webkitTransform = 'rotate('+rotation+')';
+            }, 
+            function(error){
+                console.log("error");
+            }, 
+            options
+        );
+    });
+});
